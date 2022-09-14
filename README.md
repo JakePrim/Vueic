d882cb2b0bdd70f09681b77ce56f42e960b71fa5
mac <mac@macdeMacBook-Pro.local>
2022-09-14 17:01:22 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-## summer-flutter
-
+> Reactic-Web:旨在高效率、高质量React开发中后台Web通用的解决方案及开发模版
--- flutter_bilibili/android/app/src/main/kotlin/cn/jakeprim/flutter_demo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package cn.jakeprim.flutter_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
